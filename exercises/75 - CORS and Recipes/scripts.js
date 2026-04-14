e6be9a8b0cbf3fca41cd2c98c7ff9d36f49533e5
Miguel Shin <miguelshin@Miguels-MacBook-Pro.local>
2026-04-14 17:34:45 -0600

    Recipes

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,2 +1,12 @@
 // The Recipe Puppy API used in the course is broken
 // Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
+
+const baseEndpoint = 'https://recipes.beginnerjavascript.com/api'
+
+async function fetchRecipes(query) {
+    const response = await fetch(`https://corsproxy.io/?url=${baseEndpoint}?q=${query}`);
+    const data = await response.json();
+    console.log(data);
+}
+
+fetchRecipes('pizza');
