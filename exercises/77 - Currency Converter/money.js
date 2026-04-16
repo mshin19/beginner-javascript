dc6a133b0bcf20cf8bfbc49e54d2ae0088b7c88d
Miguel Shin <miguelshin@Miguels-MacBook-Pro.local>
2026-04-15 22:59:49 -0600

    Currency Convertor

--- exercises/77 - Currency Converter/money.js
+++ exercises/77 - Currency Converter/money.js
@@ -1,3 +1,26 @@
+const fromSelect = document.querySelector('[name="from_currency"]');
+const toSelect = document.querySelector('[name="to_currency"]');
+const fromInput = document.querySelector('[name="from_amount"]');
+const toAmount = document.querySelector('.to_amount');
+const form = document.querySelector('.app form');
+const endpoint = 'https://api.apilayer.com/exchangerates_data/latest';
+const ratesByBase = {};
+
+const myHeaders = new Headers();
+myHeaders.append("apikey", "91Pb7D4nulg8KFNDv5u9EDpw5tN8qysP");
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+// fetch(`https://api.apilayer.com/exchangerates_data/latest?symbols={symbols}&base={base}`, requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+
+
 const currencies = {
   USD: 'United States Dollar',
   AUD: 'Australian Dollar',
@@ -32,3 +55,46 @@ const currencies = {
   ZAR: 'South African Rand',
   EUR: 'Euro',
 };
+
+function generateOptions(options) {
+  return Object.entries(options)
+  .map(([currencyCode, currencyName]) => {
+    return `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`;
+  })
+  .join('');
+}
+
+async function fetchRates(base = 'USD') {
+  const response = await fetch(`${endpoint}?base=${base}`, requestOptions);
+  const rates = await response.json();
+  return rates;
+}
+
+async function convert(amount, from, to) {
+  //first check if we even have the rates to convert from that currency
+  if(!ratesByBase[from]) {
+    console.log(`Oh no, we dont have ${from} to convert to ${to}. Lets get it!`);
+    const rates = await fetchRates(from);
+    console.log(rates);
+    //store them for next time
+    ratesByBase[from] = rates;
+  }
+  //convert that amount that they passed in
+  const convertedAmount = amount * ratesByBase[from].rates[to];
+  return convertedAmount;
+
+}
+
+async function handleInput(e) {
+  const rawAmount = await convert(fromInput.value, fromSelect.value, toSelect.value);
+  const formattedAmount = 
+  toAmount.textContent = rawAmount;
+}
+
+const optionsHTML = generateOptions(currencies);
+//populate the options element
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
+
+form.addEventListener('input', handleInput);
+
